22ff541a167fe584ee0c9262b67eb9321a916e02
AmirFransis <gotofransis@hotmail.com>
2014-08-02 15:36:11 +0300

    ui upgrade

---
+++ NewUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NewUI.MvcApplication" Language="C#" %>
---
+++ NewUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewUI.Startup))]
+namespace NewUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
