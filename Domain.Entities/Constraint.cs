2333d86632711c7f07bb4884fdf929611980802a
AmirFransis <gotofransis@hotmail.com>
2014-08-23 14:31:35 +0300

    fixed namespaces

---
+++ Domain.Entities/Constraint.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ShiftSchedule.Domain.Entities
+{
+    public enum ConstraintType
+    {
+        DoubleShift = 0,
+        Sandwiche = 1
+    }
+    class Constraint
+    {
+        private int id; 
+
+         
+    }
+}
